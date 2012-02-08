fc9f3f6a101f4596d67ed0c7e1ebddb6cbdcdcfb
subverse <subverse@gmx.net>
2012-02-08 20:41:25 +0100

    updated scheme

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120207192806) do
 
-  create_table "accesses", :force => true do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.integer  "level"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "accounts", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
