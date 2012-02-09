cc2a330ce0a5b260cfc2220e77eb3fffdc217f4c
subverse <subverse@gmx.net>
2012-02-09 09:18:05 +0100

    validation, .gitignore

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,11 @@
 class Account < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :user_id
+  
+  validates :name, :presence => true,
+                   :uniqueness => { :case_sensitive => false }
+                      
+  validates :user_id, :presence => true,
+                      :uniqueness => true
 end
  
